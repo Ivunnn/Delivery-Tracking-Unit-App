daa007d807ef2206359442d5eb6ef4ca08db1f37
ivunnn <herdiantoivan45@gmail.com>
2026-09-22 12:54:59 +0700

    fix:(dashboard UI)

--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -1,19 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="grid grid-cols-12 gap-4 md:gap-6">
-        <div class="col-span-12 space-y-6 xl:col-span-7">
-            <x-ecommerce.ecommerce-metrics
-                :customer-count="$customerCount"
-                :order-count="$orderCount"
-                :unit-count="$unitCount"
-                :driver-count="$driverCount"
-                :revenue="$revenue"
-            />
-        </div>
-
-        <div class="col-span-12 xl:col-span-7">
-            <x-ecommerce.recent-orders :products="$recentOrders" />
-        </div>
+  <div class="grid grid-cols-12 gap-4 md:gap-6">
+    <div class="col-span-12 space-y-6">
+      <x-ecommerce.ecommerce-metrics :customer-count="$customerCount" :order-count="$orderCount" :unit-count="$unitCount"
+        :driver-count="$driverCount" :revenue="$revenue" />
     </div>
+
+    <div class="col-span-12">
+      <x-ecommerce.recent-orders :products="$recentOrders" />
+    </div>
+  </div>
 @endsection
